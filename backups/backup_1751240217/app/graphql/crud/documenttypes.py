012e04ace0f72f9e991e6fdce65c434be12faa7e
Javi <javier_prieto@outlook.com>
2025-06-30 00:38:21 -0300

    20250630-3

--- backups/backup_1751240217/app/graphql/crud/documenttypes.py
+++
@@ -1,9 +0,0 @@
-# app/graphql/crud/documenttypes.py
-from sqlalchemy.orm import Session
-from app.models.documenttypes import DocumentTypes
-
-def get_documenttypes(db: Session):
-    return db.query(DocumentTypes).all()
-
-def get_documenttypes_by_id(db: Session, id: int):
-    return db.query(DocumentTypes).filter(DocumentTypes.documentTypeID == id).first()
