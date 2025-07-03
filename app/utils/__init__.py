0529dfcd5a89f50d263b143c0676ea0ceba691bb
javiprieto89 <javier_prieto@outlook.com>
2025-07-02 22:16:41 -0300

    Fix type errors in resolvers

--- app/utils/__init__.py
+++ app/utils/__init__.py
@@ -1,17 +1,16 @@
 from dataclasses import fields
-from typing import Any, Type, List
+from typing import Any, Type, List, Sequence
 
 # Helper functions to map SQLAlchemy models to Strawberry schemas
 
+
 def obj_to_schema(schema_type: Type[Any], obj: Any):
-    if obj is None:
-        return None
     data = {}
-    obj_dict = getattr(obj, '__dict__', {})
+    obj_dict = getattr(obj, "__dict__", {})
     for f in fields(schema_type):
         data[f.name] = obj_dict.get(f.name)
     return schema_type(**data)
 
 
-def list_to_schema(schema_type: Type[Any], objects: List[Any]):
-    return [obj_to_schema(schema_type, obj) for obj in objects]
+def list_to_schema(schema_type: Type[Any], objects: Sequence[Any]) -> List[Any]:
+    return [obj_to_schema(schema_type, obj) for obj in objects if obj is not None]
