16481f849308825c6be88f3713354d9dab582972
javiprieto89 <javier_prieto@outlook.com>
2025-07-23 17:58:37 -0300

    Fix OrderStatus foreign key

--- app/models/sysorderstatus.py
+++ app/models/sysorderstatus.py
@@ -17,11 +17,11 @@ from app.db import Base
 class SysOrderStatus(Base):  # <--- corregido nombre de clase
     __tablename__ = 'sysOrderStatus'
     __table_args__ = (
-        PrimaryKeyConstraint('OrderstatusID', name='PK__OrderSta__BC674F4170B3E561'),
+        PrimaryKeyConstraint('OrderStatusID', name='PK__OrderSta__BC674F4170B3E561'),
     )
 
-    OrderStatusID = Column("OrderstatusID", Integer, Identity(start=1, increment=1), primary_key=True)
+    OrderStatusID = Column('OrderStatusID', Integer, Identity(start=1, increment=1), primary_key=True)
     Status = Column(Unicode(50, 'Modern_Spanish_CI_AS'))
 
     # Relaciones
-    orders: Mapped[List['Orders']] = relationship('Orders', foreign_keys='[Orders.OrderStatusID]', back_populates='orderStatus_')
+    orders: Mapped[List['Orders']] = relationship('Orders', foreign_keys='Orders.OrderStatusID', back_populates='orderStatus_')
