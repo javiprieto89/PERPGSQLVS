e7cea6ec4826afd3f6be5d3eb8a3fe95fec27ded
javiprieto89 <javier_prieto@outlook.com>
2025-07-23 12:41:00 -0300

    Fix class name for SysOrderStatus

--- app/models/sysorderstatus.py
+++ app/models/sysorderstatus.py
@@ -1,4 +1,4 @@
-# ========== sysOrderStatus ===========
+# ========== SysOrderStatus ===========
 # app/models/sysorderstatus.py
 from __future__ import annotations
 from typing import Optional, TYPE_CHECKING
@@ -14,7 +14,7 @@ from sqlalchemy.orm import Mapped, relationship
 from app.db import Base
 
 
-class sysOrderStatus(Base):  # <--- corregido nombre de clase
+class SysOrderStatus(Base):  # <--- corregido nombre de clase
     __tablename__ = 'sysOrderStatus'
     __table_args__ = (
         PrimaryKeyConstraint('OrderstatusID', name='PK__OrderSta__BC674F4170B3E561'),
