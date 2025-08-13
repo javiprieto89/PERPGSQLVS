0bf8a5fb2c4642c64392db9ae7a1c146147b33d8
Adrian Lambrese <adriantg@gmail.com>
2025-08-13 12:46:46 +1000

    Add computron selfip config to vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -12,6 +12,7 @@ export default defineConfig({
     port: 5173,
     strictPort: true,
     cors: true,
+    allowedHosts: ["computron.selfip.com"],
     // Eliminamos proxy porque ahora llamamos directo a rutas como /login
   },
   resolve: {
