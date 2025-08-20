c842c24a6291cb1be6883c82569d7daf3211f9fa
javiprieto89 <javier_prieto@outlook.com>
2025-08-19 23:24:42 -0300

    Rename sysOrderStatus table mapping

--- app/models/sys/orderstatus.py
+++ app/models/sys/orderstatus.py
@@ -12,13 +12,13 @@ from app.db import Base
 
 
 class SysOrderStatus(Base):
-    __tablename__ = 'SysOrderStatus'
+    __tablename__ = 'sysOrderStatus'
     __table_args__ = (
         PrimaryKeyConstraint('OrderStatusID', name='PK__OrderSta__BC674F4170B3E561'),
     )
 
     OrderStatusID = Column(Integer, Identity(start=1, increment=1), primary_key=True)
-    Status = Column(Unicode(50, 'Modern_Spanish_CI_AS'))
+    Status = Column(Unicode(50, 'Modern_Spanish_CI_AS'), nullable=False)
 
     # Relaciones (solo lectura)
     orders: Mapped[List['Orders']] = relationship('Orders', foreign_keys='Orders.OrderStatusID', back_populates='orderStatus_')
