3de742af935ae50bb6b89ef7d94598873c3eefef
javiprieto89 <javier_prieto@outlook.com>
2025-08-19 23:10:30 -0300

    Update sysDocTypes model and consistency report

--- app/models/sys/doctypes.py
+++ app/models/sys/doctypes.py
@@ -13,14 +13,14 @@ from app.db import Base
 
 
 class SysDocTypes(Base):
-    __tablename__ = 'SysDocTypes'
+    __tablename__ = 'sysDocTypes'
     __table_args__ = (
         PrimaryKeyConstraint('DocTypeID', name='PK__SysDocTypes__055E26832A8E0FF3'),
     )
 
     DocTypeID = Column(Integer, Identity(start=1, increment=1), primary_key=True)
-    Name = Column(Unicode(100, 'Modern_Spanish_CI_AS'))
-    IsActive = Column(Boolean, server_default=text('((1))'))
+    Name = Column(Unicode(100, 'Modern_Spanish_CI_AS'), nullable=False)
+    IsActive = Column(Boolean, nullable=False, server_default=text('((1))'))
 
     # Relaciones (solo lectura)
     clients: Mapped[List['Clients']] = relationship('Clients', back_populates='docTypes_')
