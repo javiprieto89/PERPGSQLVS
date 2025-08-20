dadaa1728ceafab90b7a46a6e4cfaef97d17b039
javiprieto89 <javier_prieto@outlook.com>
2025-08-19 21:08:19 -0300

    Add CompanyID to credit cards

--- app/models/creditcards.py
+++ app/models/creditcards.py
@@ -19,12 +19,14 @@ class CreditCards(Base):
     __tablename__ = 'CreditCards'
     __table_args__ = (
         ForeignKeyConstraint(['CreditCardGroupID'], ['CreditCardGroups.CreditCardGroupID'], name='FK_CreditCards_CardGroups'),
+        ForeignKeyConstraint(['CompanyID'], ['CompanyData.CompanyID'], name='FK_CreditCards_CompanyData'),
         PrimaryKeyConstraint('CreditCardID', name='PK_CreditCards')
     )
 
     CreditCardID = Column(Integer, Identity(start=1, increment=1), primary_key=True)
     CardName = Column(Unicode(100, 'Modern_Spanish_CI_AS'))
     CreditCardGroupID = Column(Integer)
+    CompanyID = Column(Integer)
     IsActive = Column(Boolean, server_default=text('((1))'))
     Surcharge = Column(DECIMAL(18, 4), server_default=text('((0))'))
     Installments = Column(Integer)
